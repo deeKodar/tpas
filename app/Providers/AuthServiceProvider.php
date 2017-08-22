c121cf247f5bd5a0353826883c52d5be2a2c5afa
deeKodar <darpuntha02@gmail.com>
2017-08-22 14:57:34 +0600

    minor auth service conflict merge error removed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -44,11 +44,9 @@ class AuthServiceProvider extends ServiceProvider
                     return $user->hasRole($permission->roles);
             });
         }
-<<<<<<< HEAD
-=======
 
->>>>>>> e6708ce12cba75676d7570c37529763096970c0b
     }
+    
     protected function getPermissions() {
 
         return Permission::with('roles')->get();
